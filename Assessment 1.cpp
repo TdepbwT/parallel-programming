e07088650a6e0db1f83db8f9fbd106d7ce39d561
Opemipo Adeoyo <108328249+TdepbwT@users.noreply.github.com>
2025-04-03 18:29:13 +0100

    setup

--- Assessment 1.cpp
+++ Assessment 1.cpp
@@ -1,5 +1,23 @@
 /*
 David A - 26321509
+blah
 */
 
 #include <iostream>
+#include <vector>
+
+#include "include/Utils.h"
+#include "include/CImg.h"
+
+using namespace cimg_library;
+
+void print_help() {
+	std::cerr << "Application usage:" << std::endl;
+
+	std::cerr << "  -p : select platform " << std::endl;
+	std::cerr << "  -d : select device" << std::endl;
+	std::cerr << "  -l : list all platforms and devices" << std::endl;
+	std::cerr << "  -f : input image file (default: test.ppm)" << std::endl;
+	std::cerr << "  -h : print this message" << std::endl;
+}
+
